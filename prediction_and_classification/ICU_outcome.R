920767d1cd8dc6234fdce1c0b5cd07907325cfaf
Nikolai Treschow Kühl <s221088@dtu.dk>
2026-01-23 10:23:30 +0100

    Added evaluation to the model

--- prediction_and_classification/ICU_outcome.R
+++ prediction_and_classification/ICU_outcome.R
@@ -5,6 +5,7 @@ library(tidyverse) #For loading CSV file
 library(caTools) #For splitting into train and test
 library(ggplot2)
 library(reshape2)
+library(pROC)
 
 # Set random seed
 set.seed(999)
@@ -13,8 +14,11 @@ set.seed(999)
 # Load data
 ###################################################
 
-features_file_path <- "L:/Auditdata/CONNECT-ME/Nikolai/pupillometry/Data/pupilometry_features.csv"
-outcome_file_path <- "L:/Auditdata/CONNECT-ME/Nikolai/pupillometry/Data/ICU_outcome.csv"
+# features_file_path <- "L:/Auditdata/CONNECT-ME/Nikolai/pupillometry/Data/pupilometry_features.csv"
+features_file_path <- "C:/Users/NTres/OneDrive - Danmarks Tekniske Universitet/Arbejde_Rigshospitalet/Pupillometry/Data/Extracted_features/pupilometry_features.csv"
+# outcome_file_path <- "L:/Auditdata/CONNECT-ME/Nikolai/pupillometry/Data/ICU_outcome.csv"
+outcome_file_path <- "C:/Users/NTres/OneDrive - Danmarks Tekniske Universitet/Arbejde_Rigshospitalet/Pupillometry/Data/Extracted_features/ICU_outcome.csv"
+
 features <- read_csv(features_file_path, show_col_types = FALSE) %>% select(-`...1`) %>% select(-`date_examination_merged`)
 ICU_outcome <-  read_csv(outcome_file_path, show_col_types = FALSE) %>% select(record_id, ICU_outcome) %>% mutate(ICU_outcome = ifelse(ICU_outcome == "A", 1, 0))#Rename ICU outcome to 0/1
 
@@ -74,32 +78,31 @@ predictions <- data.frame(
                           actual_class = testing_data_scaled$ICU_outcome
                           )
 
+###################################################
+# Baseline model: Majority class classifier
+###################################################
+
+# Compute class probability from training data
+majority_classifier <- sum(training_data_scaled$ICU_outcome) / length(training_data_scaled$ICU_outcome)
+majority_class <- ifelse(majority_classifier > 0.5, 1, 0)
+
+# Define classes
+majority_predictions <- rep(majority_class, times = length(testing_data_scaled$ICU_outcome))
 
 ###################################################
 # Evaluation
 ###################################################
+#Plot the distribution of the outcome variable
+barplot(table(training_data_scaled$ICU_outcome),
+        xlab = "ICU outcome",
+        ylab = "Count",
+        main = "Distribution of ICU outcome")
+
 # Creating confusion matrix
-conf_matrix <- table(Predicted = predict_class, Actual = testing_data_scaled$ICU_outcome)
-
-# Extracting evaluation metrics
-TP <- conf_matrix[2, 2]
-TN <- conf_matrix[1, 1]
-FP <- conf_matrix[2, 1]
-FN <- conf_matrix[1, 2]
-
-# Compute evaluation metrics
-precision <- TP / (TP + FP)
-recall <- TP / (TP + FN)
-accuracy <- (TP + TN) / (TP + TN + FP + FN)
-f1_score <- 2 * (precision * recall) / (precision + recall)
-
-cat("Precision:", precision, "\n")
-cat("Recall:", recall, "\n")
-cat("Accuracy:", accuracy, "\n")
-cat("F1 Score:", f1_score, "\n")
+log_reg_conf_matrix <- table(Predicted = predict_class, Actual = testing_data_scaled$ICU_outcome)
 
 # Reshape the confusion matrix for ggplot2
-conf_matrix_melted <- as.data.frame(conf_matrix)
+conf_matrix_melted <- as.data.frame(log_reg_conf_matrix)
 colnames(conf_matrix_melted) <- c("Actual", "Predicted", "Count")
 
 ggplot(conf_matrix_melted, aes(x = Actual, y = Predicted, fill = Count)) +
@@ -108,3 +111,100 @@ ggplot(conf_matrix_melted, aes(x = Actual, y = Predicted, fill = Count)) +
   scale_fill_gradient(low = "white", high = "blue") +
   labs(title = "Confusion Matrix Heatmap", x = "Actual", y = "Predicted") +
   theme_minimal()
+
+# Extracting evaluation metrics
+log_reg_TP <- log_reg_conf_matrix[2, 2]
+log_reg_TN <- log_reg_conf_matrix[1, 1]
+log_reg_FP <- log_reg_conf_matrix[2, 1]
+log_reg_FN <- log_reg_conf_matrix[1, 2]
+
+# Compute evaluation metrics
+log_reg_precision <- log_reg_TP / (log_reg_TP + log_reg_FP)
+log_reg_recall <- log_reg_TP / (log_reg_TP + log_reg_FN)
+log_reg_accuracy <- (log_reg_TP + log_reg_TN) / (log_reg_TP + log_reg_TN + log_reg_FP + log_reg_FN)
+log_reg_f1_score <- 2 * (log_reg_precision * log_reg_recall) / (log_reg_precision + log_reg_recall)
+
+
+
+
+# Creating confusion matrix
+majority_conf_matrix <- table(
+  Predicted = factor(majority_predictions, levels = c(0, 1)),
+  Actual    = factor(testing_data_scaled$ICU_outcome, levels = c(0, 1))
+)
+
+# Reshape the confusion matrix for ggplot2
+conf_matrix_melted <- as.data.frame(majority_conf_matrix)
+colnames(conf_matrix_melted) <- c("Actual", "Predicted", "Count")
+
+ggplot(conf_matrix_melted, aes(x = Actual, y = Predicted, fill = Count)) +
+  geom_tile() +
+  geom_text(aes(label = Count), color = "black", size = 6) +  # Add text labels
+  scale_fill_gradient(low = "white", high = "blue") +
+  labs(title = "Confusion Matrix Heatmap", x = "Actual", y = "Predicted") +
+  theme_minimal()
+
+# Extracting evaluation metrics
+majority_TP <- majority_conf_matrix[2, 2]
+majority_TN <- majority_conf_matrix[1, 1]
+majority_FP <- majority_conf_matrix[2, 1]
+majority_FN <- majority_conf_matrix[1, 2]
+
+# Compute evaluation metrics
+majority_precision <- majority_TP / (majority_TP + majority_FP)
+majority_recall <- majority_TP / (majority_TP + majority_FN)
+majority_accuracy <- (majority_TP + majority_TN) / (majority_TP + majority_TN + majority_FP + majority_FN)
+majority_f1_score <- 2 * (majority_precision * majority_recall) / (majority_precision + majority_recall)
+
+
+cat("Majority Precision:", majority_precision, "\n")
+cat("Majority Recall:", majority_recall, "\n")
+cat("Majority Accuracy:", majority_accuracy, "\n")
+cat("Majority F1 Score:", majority_f1_score, "\n")
+
+cat("Log Reg Precision:", log_reg_precision, "\n")
+cat("Log Reg Recall:", log_reg_recall, "\n")
+cat("Log Reg Accuracy:", log_reg_accuracy, "\n")
+cat("Log Reg F1 Score:", log_reg_f1_score, "\n")
+
+roc_logreg <- roc(
+  response = testing_data_scaled$ICU_outcome,
+  predictor = predict_prob,
+  levels = c(0, 1),
+  direction = "<"
+)
+
+auc_logreg <- auc(roc_logreg)
+ci_auc <- ci.auc(roc_logreg, method = "delong")
+
+auc_logreg
+ci_auc
+
+plot(
+  roc_logreg,
+  col = "blue",
+  lwd = 2,
+  main = "ROC curve - Day-1 logistic regression"
+)
+
+roc_majority <- roc(
+  response = testing_data_scaled$ICU_outcome,
+  predictor = majority_predictions,
+  levels = c(0, 1),
+  direction = "<"
+)
+
+auc_majority <- auc(roc_majority)
+ci_auc <- ci.auc(roc_majority, method = "delong")
+
+auc_majority
+ci_auc
+
+plot(
+  roc_majority,
+  col = "blue",
+  lwd = 2,
+  main = "ROC curve - Day-1 logistic regression"
+)
+
+roc.test(roc_logreg, roc_majority, method = "delong")
