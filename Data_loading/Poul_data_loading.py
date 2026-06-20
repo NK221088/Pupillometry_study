c976f39148c58f6b91f08c52c0ad775415ccb5c6
Nikolai Treschow Kühl <s221088@dtu.dk>
2026-06-20 12:33:08 +0200

    Added rounding to interpolated values

--- Data_loading/Poul_data_loading.py
+++ Data_loading/Poul_data_loading.py
@@ -38,7 +38,7 @@ for file in all_files:
     df["PatientID"] = df["PatientID"].astype(str).str.zfill(10)
     print(f"Discarded {len_before - len(df)} rows with non-numeric PatientID in file: {file.split('\\')[-1]}")
     df[ts_cols] = df[ts_cols].astype(float)
-    df[ts_cols] = df[ts_cols].apply(interpolate_zeros, axis=1, result_type='expand')
+    df[ts_cols] = df[ts_cols].apply(interpolate_zeros, axis=1, result_type='expand').round(2)
     len_before = len(df)
     df = df.drop_duplicates(subset=['DateTime', 'PatientID', 'Pupil-Measured'], keep='first')
     print(f"Discarded {len_before - len(df)} duplicate rows in file: {file.split('\\')[-1]}")
